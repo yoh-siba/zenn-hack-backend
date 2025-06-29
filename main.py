c5235e5b36e3f61ea2a51e795c3ee6d083106fb8
nobu74658 <hiroki-pon@outlook.jp>
2025-06-29 22:13:14 +0900

    fix: CORS設定

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://zenn-hack-backend.web.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
